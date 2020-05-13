925176bca63cd18c59fbbe6cb3f4c044f97aa3f0
Timme Bethe <tibe@riseup.net>
2020-05-13 17:45:43 +0200

    Updated some settings to make it work with my local mysql server where i created a database called atlantisdb and a corresponding user.

--- atlantisdatabases/settings.py
+++ atlantisdatabases/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'elective_search',
+    'home',
+    'information_pages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -70,13 +73,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'atlantisdatabases.wsgi.application'
 
 
-# Database
+# Database            ssion: 12-05-2020, 13:45-17:30
+
+
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'atlantisdb',
+        'USER': 'atlantisdb',
+        'PASSWORD': 'temp',
     }
 }
 
